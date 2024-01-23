d6047b66173cdf733cac982d997a427c820c21be
mayyashar <may27499@gmail.com>
2024-01-23 19:03:02 +0200

    with frontes

---
+++ main.py
@@ -0,0 +1,101 @@
+from fastapi import Depends, FastAPI, Request
+import numpy as np
+from typing import List, Union
+from pydantic import BaseModel
+import psycopg2
+from fastapi.responses import HTMLResponse
+from fastapi import Depends
+from fastapi.templating import Jinja2Templates
+
+DATABASE_URL = "postgresql://postgres:root@y172.17.0.2:5432/postgres"
+
+def get_connection():
+    connection = psycopg2.connect(DATABASE_URL)
+    try:
+        yield connection
+    finally:
+        connection.close()
+
+print("May - postgres connected")
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+
+templates =Jinja2Templates(directory="templates")
+
+@app.get("/")
+def index():
+    return {"greeting": "Hello, everything is up and running!"}
+
+@app.get("/index", response_class=HTMLResponse)
+def index2(request: Request):
+    clothes= [
+            {'name':'tshirt', 'size':'S'}
+
+    ]
+    
+    context={'request': request, 'clothes':clothes}
+    return templates.TemplateResponse("index.html", context)
+
+        # user = 'postgres', password='root', host='172.17.0.2', port='5432')
+
+
+
+# from models import RequestBody, ResponseBody
+
+
+
+
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Union[str, None] = None):
+#     return {"item_id": item_id, "q": q}
+
+# @app.get('/colors') )
+# def read_item(item_id: int, q: Union[str, None] = None):
+#     return {"item_id": item_id, "q": q}
+
+
+# @app.put("/items/{item_id}")
+# def update_item(item_id: int, item: Item):
+#     return {"item_name": item.name, "item_id": item_id}
+
+# @app.post("/items/{item_id}")
+# def update_item(item_id: int, item: Item):
+#     return {"item_name": item.name, "item_id": item_id}
+
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: str):
+#     return {"item_id": item_id}
+
+# async def get_shop_html():
+#     with open(f".\\frontend\\may_shop.html", "r") as file:
+#         return file.read()
+
+
+
+
+
+# @app.post("/items/{item_id}")
+# def create_item(item: dict, connection: psycopg2.extensions.connection = Depends(get_connection)):
+#     try:
+#         with connection.cursor() as cursor:
+#             insert_query = "INSERT INTO items (name, description) VALUES (%s, %s) RETURNING id;"
+#             cursor.execute(insert_query, (item["name"], item["description"]))
+#             item_id = cursor.fetchone()[0]
+#             connection.commit()
+#             return {"id":item_id, **item}
+        
+#     except Exception as e:
+#             raise NameError
+
+    
+
+
+
+
+
+# @app.post("/dot")
